82b82f484181f9c342639f1b625885fb218d2d05
Derek <drc3141@gmail.com>
2023-11-29 00:11:15 -0700

    revised SearchDatabase class including logic to search for complements

--- src/main/java/cs321/search/GeneBankSearchDatabase.java
+++ src/main/java/cs321/search/GeneBankSearchDatabase.java
@@ -2,9 +2,6 @@ package cs321.search;
 
 
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,28 +11,50 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-
-import cs321.btree.BTreeException;
 import cs321.common.ParseArgumentException;
+import cs321.create.GeneBankCreateBTreeArguments;
+import cs321.create.SequenceUtils;
 
+
+
+
+/**
+ * A program that searches a given SQLite database created alongside BTree.
+ * <p>
+ * Run `java -jar build/libs/GeneBankSearchDatabase.jar --help` for usage information.
+ *
+ * @author Derek Caplinger
+ * @author Matt Youngberg
+ */
 public class GeneBankSearchDatabase
 {
 
-    public static void main(String[] args) throws Exception
-    {
-        //System.out.println("Hello world from cs321.search.GeneBankSearchDatabase.main");
-        GeneBankSearchDatabaseArguments geneBankSearchDatabaseArguments = parseArgumentsAndHandleExceptions(args);
-        searchDatabase(geneBankSearchDatabaseArguments);
+	/**
+	 * The entry point for the GeneBankSearchDatabase program
+	 * 
+	 * This method will parse the command line arguments and then search the provided SQLite database based on the given query file
+	 * 
+	 * @param args				command line arguments to database path and query file
+	 * @throws SQLException		If there is an error in the SQLite connection
+	 * @throws IOException		if there is an error reading the query file
+	 */
+	public static void main(String[] args) throws SQLException, IOException
+	{
+		//System.out.println("Hello world from cs321.search.GeneBankSearchDatabase.main");
+		GeneBankSearchDatabaseArguments geneBankSearchDatabaseArguments = parseArgumentsAndHandleExceptions(args);
+		searchDatabase(geneBankSearchDatabaseArguments);
 
-    }
-    
-private static GeneBankSearchDatabaseArguments parseArgumentsAndHandleExceptions(String[] args) {
-		
+	}
+
+	/**
+	 * This method takes in the command line arguments and will parse the arguments to return an object containing the
+	 * appropriate arguments or print a usage method and exit the program
+	 *
+	 * @param args command line arguments provided with program call
+	 * @return validated {@link GeneBankSearchDatabaseArguments} object
+	 */
+	private static GeneBankSearchDatabaseArguments parseArgumentsAndHandleExceptions(String[] args) {
+
 		GeneBankSearchDatabaseArguments geneBankSearchDatabaseArguments = null;
 		try
 		{
@@ -44,151 +63,109 @@ private static GeneBankSearchDatabaseArguments parseArgumentsAndHandleExceptions
 		catch (ParseArgumentException e) {
 			printUsageAndExit(e.getMessage());
 		}
-		
+
 		return geneBankSearchDatabaseArguments;
 	}
-
+	
+	/**
+     * This method prints when invalid arguments are detected and will print an appropriately formatted usage example
+     * and exit the program.
+     *
+     * @param errorMessage error message provided by parse argument exception
+     * @param exitCode     exit code to be used when exiting the program
+     */
 	private static void printUsageAndExit(String errorMessage) {
-		
+
 		System.out.println(errorMessage);
-	    System.out.println("Usage: java -jar build/libs/GeneBankSearchDatabase.jar --database=<SQLite-database-path> ");
-	    System.out.println("\t --queryfile=<query-file>");
-	    System.exit(1);
-	    
+		System.out.println("Usage: java -jar build/libs/GeneBankSearchDatabase.jar --database=<SQLite-database-path> ");
+		System.out.println("\t --queryfile=<query-file>");
+		System.exit(1);
+
 	}
 
+	/**
+     * This method accepts the command line arguments, args, and attempts to parse the arguments on position first and,
+     * if not able, will attempt to parse arguments by name.
+     * <p>
+     * Invalid argument entries will throw a ParseArgumentException to be handled in generating error message.
+     *
+     * @param args command line arguments provided by user.
+     * @throws ParseArgumentException exception issue encountered parsing arguments
+     * @return validate {@link GeneBankSearchDatabaseArguments} object
+     */ 
 	private static GeneBankSearchDatabaseArguments parseArguments(String[] args) throws ParseArgumentException {
 		try {
-            return parsePositionalArgs(args);  // try parsing as positional first; tests require it
-        } catch (ParseArgumentException e) {
-            // ignore; parse as named args
-        }
+			return GeneBankSearchDatabaseArguments.fromStringArgs(args);  
+		} catch (IllegalArgumentException e) {
+			throw new ParseArgumentException(e.getMessage());
+		}
+	}	
 
-        try {
-            return parseNamedArgs(args);  // try parsing as named arguments
-        } catch (ParseArgumentException e) {
-            throw new ParseArgumentException(e.getMessage());  // No fallback if this doesn't work
-        }
-	}
-	
-	private static GeneBankSearchDatabaseArguments parsePositionalArgs(String[] args) throws ParseArgumentException {
-		if (args.length != 2) {
-            throw new ParseArgumentException("Invalid arguments. Required: SQLite-database-path queryfile");
-        }
-
-        try {
-            // parse 'SQLite-database-path' and getPath using file name
-            Path pathValue = FileSystems.getDefault().getPath(args[0]);
-            
-            
-            // Don't validate 'queryfile' here; given tests can't handle it.
-            // It will be validated when it is opened
-            String queryFileValue = args[1];
-
-
-            
-
-            return new GeneBankSearchDatabaseArguments(
-                    pathValue,
-                    queryFileValue
-            );
-
-        } catch (InvalidPathException e) {
-            throw new ParseArgumentException("Invalid Path " + e.getMessage());
-        } 
-	}
-
-	private static GeneBankSearchDatabaseArguments parseNamedArgs(String[] args) throws ParseArgumentException {
-		Options options = new Options();
-
-        // DatabasePath
-        Option databasePath = new Option("database", true, "Path to SQLite database");
-        databasePath.setRequired(true);
-        options.addOption(databasePath);
-        
-    	// queryfile
-        Option queryfile = new Option("queryfile", true, "Path to the input query file");
-        queryfile.setRequired(true);
-        options.addOption(queryfile);
-
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd;
-
-        try {
-            cmd = parser.parse(options, args);
-
-            
-           
-            Path databasePathValue = FileSystems.getDefault().getPath(cmd.getOptionValue("database"));
-
-            // Don't validate 'queryfile' here; given tests can't handle it.
-            // It will be validated when it is opened
-            String queryfileValue = cmd.getOptionValue("queryfile");
-
-        } catch (Exception e) {
-            throw new ParseArgumentException(e.getMessage());
-        }
-
-        return new GeneBankSearchDatabaseArguments(
-        		FileSystems.getDefault().getPath(cmd.getOptionValue("database")),
-                cmd.getOptionValue("queryfile")
-                
-        );
-	}
-
+	/**
+	 * This is the driving method of the GeneBankSearchDatabase class. Using the provided arguments it connects to a SQLite database created
+	 * alongside a BTree file and uses the provided Query file to print the results of the search against the provided database. 
+	 * 
+	 * @param args				Parsed and validated command line arguments provided by user 
+	 * @throws SQLException		if an error occurred with the database connection
+	 * @throws IOException		if an error occured reading the query file			
+	 */
 	private static void searchDatabase(GeneBankSearchDatabaseArguments args) throws SQLException, IOException {
-		Connection connection = null;
-
-		
-			String databasePath = args.getDatabasePath().toString();
-			//verify file exists before trying to open
-			Path filePath = Paths.get(databasePath);
-
-			// check this file exists
-			if(!filePath.toFile().exists()) {
-				throw new IOException("Database file does not exist or was not located");
-			}
-			
-			try {//open SQlite connection
-			connection = DriverManager.getConnection("jdbc:sqlite:" + databasePath);
+		String databasePath = args.getDatabasePath().toString();
+		try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + databasePath)){
 
 			//create scanner to read query file
 			Scanner input = new Scanner(Paths.get(args.getQueryFileName()));
 			String subsequenceString = null;
+			String subseqComplementString = null; 
 
 			//define query and prepared statement for repeated searches
-			String query = "SELECT * FROM Sequences WHERE Encoding = ?";
+			String query = "SELECT * FROM Sequences WHERE Encoding = ? OR Encoding = ?";
 			PreparedStatement pstmt = connection.prepareStatement(query);
 
 			//execute database searches and process results set to print output 
 			while (input.hasNext()) {
 				subsequenceString = input.nextLine().trim();
+				subseqComplementString = getComplement(subsequenceString);
 				pstmt.setString(1, subsequenceString);
+				pstmt.setString(2, subseqComplementString);
 				ResultSet results = pstmt.executeQuery();
 
 
 				System.out.println("Search Results");
 				printSearchResults(results);
-		
 			}
-			} finally {
-				if (connection != null) {
-					connection.close();
-				}
-			}
+		}
+	}
 
 	
+	/**
+	 * This helper method takes in a returned result set for a database query and prints all columns of the result set to standard output
+	 * 
+	 * @param results			result set returned from SQLite query statement
+	 * @throws SQLException		if an error occurred in the SQLite connection
+	 */
+	private static void printSearchResults(ResultSet results) throws SQLException {
+		ResultSetMetaData resultsData = results.getMetaData();
+		int numCol = resultsData.getColumnCount();
+		while (results.next()) {
+			for (int i = 1; i <= numCol; i++) {
+				System.out.print(results.getString(i) + " ");
+			}
+			System.out.println();
+		}
 	}
 	
-	  private static void printSearchResults(ResultSet results) throws SQLException {
-	        ResultSetMetaData resultsData = results.getMetaData();
-	        int numCol = resultsData.getColumnCount();
-	        while (results.next()) {
-	            for (int i = 1; i <= numCol; i++) {
-	                System.out.print(results.getString(i) + " ");
-	            }
-	            System.out.println();
-	        }
-	    }
+	
+	/**
+	 * A helper method that takes in a String representation of a DNA subsequence and returns a String representation of its complement
+	 * 
+	 * @param origSubsequence	String representation of a DNA subsequence 
+	 * @return					String representation of the Complement to the provided DNA Subsequence. 
+	 */
+	private static String getComplement(String origSubsequence) {
+		int subsequenceLength = origSubsequence.length();
+		long subsequenceComplement = SequenceUtils.getComplement(SequenceUtils.dnaStringToLong(origSubsequence), subsequenceLength);
+		return SequenceUtils.longToDnaString(subsequenceComplement, subsequenceLength);
+	}
 
 }
--- src/main/java/cs321/search/GeneBankSearchDatabaseArguments.java
+++ src/main/java/cs321/search/GeneBankSearchDatabaseArguments.java
@@ -1,12 +1,59 @@
 package cs321.search;
 
+import java.io.File;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+
+
+/**
+* A class to represent the arguments to the GeneBankSearchDatabase program.
+* <p>
+* Create a new instance via the factory method {@link #fromStringArgs(String[])}. This will parse the string array of
+* arguments from the command line, run validation, and return a new {@link GeneBankSearchDataBaseArguments} object.
+* <p>
+* The usage, per the project spec, is as follows:
+* <p>
+* <pre>
+* java -jar  java -jar build/libs/GeneBankSearchDatabase.jar --database=<SQLite-database-path> --queryfile=<query-file>
+* </pre>
+*
+* @author Derek Caplinger
+* @author Matt Youngberg
+*/
 public class GeneBankSearchDatabaseArguments
 {
-	private final Path databasePath;
-	private final String queryFileName;
+	/**
+     * A pattern that string arguments must match.
+     */
+    private static final Pattern argPattern = Pattern.compile("^--([a-zA-Z]+)=([0-9a-zA-Z._/\\\\:-]+)$");
 
+    /**
+     * A Path object to the location of the SQLite Database created alongside the BTree.
+     */
+	private final Path databasePath;
+	
+	/**
+	 * The file name of the Query file. Should exist on disk
+	 */
+	private final String queryFileName;
+	
+	
+	/**
+     * Create a new {@link GeneBankSearchDatabaseArguments} object.
+     *
+     * @param databasePath 		The path to the SQLite database created alongside BTree
+     * @param queryFileName		the file name of the Query file. Should exist on disk
+     *
+     */
     public GeneBankSearchDatabaseArguments(Path databasePath, String queryFileName)
     {
         this.databasePath = databasePath;
@@ -85,6 +132,70 @@ public class GeneBankSearchDatabaseArguments
 	public String getQueryFileName() {
 		return queryFileName;
 	}
+	
+	 /**
+     * Create a GeneBankSearchDatabaseArguments object from a string array of arguments, typically the command line args.
+     * <p>
+     * Similarly to GeneBankCreateBTreeArguments this method has been added to the arguments class that manages what the arguments should be 
+     * to be parsed and validated 
+     * 
+     * @param args  the string array of arguments
+     * @return      a GeneBankSearchDatabaseArguments object
+     */
+    public static GeneBankSearchDatabaseArguments fromStringArgs(String[] args) {
+        Map<String, String> argMap = new HashMap<>();
+
+        Set<String> requiredArgs = new HashSet<>(Arrays.asList("database", "queryfile"));
+
+        // Get all strings into argMap
+        Matcher matcher;
+        for (String argStr : args) {
+            matcher = argPattern.matcher(argStr);
+            if (!matcher.matches()) {
+                throw new IllegalArgumentException("Invalid argument: " + argStr);
+            }
+
+            String[] argComponents = argStr.split("=");
+            String param = argComponents[0].substring(2);
+            String arg = argComponents[1];
+
+            if (!requiredArgs.contains(param)) {
+                throw new IllegalArgumentException("Invalid argument: " + argStr);
+            } else {
+                argMap.put(param, arg);
+            }
+        }
+
+        // Check for required arguments
+        for (String requiredArg : requiredArgs) {
+            if (!argMap.containsKey(requiredArg)) {
+                throw new IllegalArgumentException("Missing required argument: " + requiredArg);
+            }
+        }
+
+        // Check for invalid arguments and consistency
+
+        // database path
+        String databasePathString = argMap.get("database");
+        try {
+        	Path databasePath = Paths.get(databasePathString);
+        }catch(InvalidPathException e) {
+        	throw new IllegalArgumentException("Invalid Path argument: " + databasePathString + " unable to be resolved.");
+        }
+
+        // queryfile 
+        String queryFileValue = argMap.get("queryfile");
+        File queryfile = new File(queryFileValue);
+        if (!queryfile.exists()) {
+            throw new IllegalArgumentException("Invalid argument. query file not found: queryfile=" + queryFileValue);
+        }
+
+        // Create and return the arguments object
+        return new GeneBankSearchDatabaseArguments(
+                Paths.get(databasePathString),
+                argMap.get("queryfile")
+        );
+    }
 
 
 }
